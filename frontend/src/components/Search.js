293f711b3932e929f1af36bb36d819d585622ccd
kwarsi858 <kwarsi97@gmail.com>
2023-04-07 11:47:02 -0400

    added state to search component

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+const Search = ({word, setWord, handleSubmit}) => {
+
+    return (
+
+        <Container className="mt-4">
+            <Row className="justify-content-center">
+                <Col xs={12} md={8} lg={6}>
+                    <Form onSubmit={handleSubmit}>
+                        <Row>
+                            <Col xs={9}>
+                                <Form.Control 
+                                type="text"
+                                value={word}
+                                onChange={(e)=>setWord(e.target.value)}
+                                placeholder="Search for new image ..." />
+                            </Col>
+                            <Col>
+                                <Button variant="primary" type="submit">Search</Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+
+    )
+
+
+};
+
+export default Search; 
