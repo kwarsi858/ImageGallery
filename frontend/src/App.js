7693dc6cf84643af0adaa385d178ed8c6a02556e
kwarsi858 <kwarsi97@gmail.com>
2023-06-13 13:33:35 -0400

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,40 @@
-import {useState} from 'react'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search'; 
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY; 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-const App = ()=> {
+const App = () => {
+  const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
 
-  const [word,setWord] = useState('')
+  console.log(images)
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault(); 
-    console.log(word)
+    e.preventDefault();
+    console.log(word);
 
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res)=>res.json())
       .then((data)=>{
-        console.log(data)
+        setImages([data, ...images])
       })
-      .catch((err) => {
-        console.log(err); 
+      .catch((err)=>{
+        console.log(err)
       })
 
-      setWord('')
-  
-  }
-
+    setWord("");
+  };
 
   return (
     <div className="App">
       <Header title="Images Gallery 2" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
